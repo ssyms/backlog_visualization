ea7fc61e4109adb8ffbd602fb15f52e52616672d
Sarada S <symonds.sarada@gmail.com>
2019-01-08 00:48:40 -0500

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res){
+  res.render('index', {
+    title: 'End the Backlog'
+  });
+});
+
+module.exports = router;
