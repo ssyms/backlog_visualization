8892c3eac18ff607a4ce105d01777d1c6e4e7abd
Sarada S <symonds.sarada@gmail.com>
2019-01-08 00:53:55 -0500

    minor color edits

--- public/javascripts/data-vis.js
+++ public/javascripts/data-vis.js
@@ -1,23 +1,20 @@
 var width = 960;
 var height = 500;
 
-// D3 Projection
 var projection = d3.geo.albersUsa()
-				   .translate([width/2, height/2])    // translate to center of screen
-				   .scale([1000]);          // scale things down so see entire US
-
-// Define path generator
-var path = d3.geo.path()               // path generator that will convert GeoJSON to SVG paths
-		  	 .projection(projection);  // tell path generator to use albersUsa projection
+				   .translate([width/2, height/2])
+				   .scale([1000]);
 
 
-// Define linear scale for output
+var path = d3.geo.path()
+		  	 .projection(projection);
+
 var color = d3.scale.linear()
 			  .range(["rgb(192,214,228)", "rgb(202,234,231)","rgb(131,208,201)","rgb(101,195,186)", "rgb(53,167,156)", "rgb(42,133,124)", "rgb(0,90,81)"]);
 
 var legendText = ["Comprehensive Reform", "", "", "", "", "", "No Reform"];
 
-//Create SVG element and append map to the SVG
+
 var svg = d3.select("body")
 			.append("svg")
 			.attr("width", width)
@@ -25,44 +22,36 @@ var svg = d3.select("body")
       .attr("margin", "auto")
       .attr("display", "block");
 
-// Append Div for tooltip to SVG
+
 var div = d3.select("body")
 		    .append("div")
     		.attr("class", "tooltip")
     		.style("opacity", 0);
 
-// Load in my states data!
 d3.csv("backlog_data.csv", function(data) {
-color.domain([0,1,2,3,4,5,6]); // setting the range of the input data
+color.domain([0,1,2,3,4,5,6]);
 
-// Load GeoJSON data and merge with states data
 d3.json("us-states.json", function(json) {
 
 // Loop through each state data value in the .csv file
 for (var i = 0; i < data.length; i++) {
 
-	// Grab State Name
 	var dataState = data[i].state;
 
-	// Grab data value
 	var dataValue = data[i].total;
 
-	// Find the corresponding state inside the GeoJSON
 	for (var j = 0; j < json.features.length; j++)  {
 		var jsonState = json.features[j].properties.name;
 
 		if (dataState == jsonState) {
 
-		// Copy the data value into the JSON
 		json.features[j].properties.total = dataValue;
 
-		// Stop looking through the JSON
 		break;
 		}
 	}
 }
 
-// Bind the data to the SVG and create one path per GeoJSON feature
 svg.selectAll("path")
 	.data(json.features)
 	.enter()
@@ -84,7 +73,6 @@ svg.selectAll("path")
 	}
 });
 
-// Modified Legend Code from Mike Bostock: http://bl.ocks.org/mbostock/3888852
 var legend = d3.select("body").append("svg")
       			.attr("class", "legend")
      			.attr("width", 200)
